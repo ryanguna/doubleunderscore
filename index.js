f3fb5b6231f4843897974ff36d7e46f5bb1b701a
Ryan Gunawardana <ryanmgunawardana@gmail.com>
2018-05-16 17:18:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports  = {
-    __stringWithDate :  require('./modules/__stringWithDate'),
-    __longestWord    :  require('./modules/__longestWord'),
-    __factorial      :  require('./modules/__factorial'),
-    __reverseString  :  require('./modules/__reverseString')
+    __stringWithDate          :  require('./modules/__stringWithDate'),
+    __getLongestWord          :  require('./modules/__getLongestWord'),
+    __getFactorial            :  require('./modules/__getFactorial'),
+    __reverseString           :  require('./modules/__reverseString'),
+    __summationNotation       :  require('./modules/__summationNotation'),
+    __capitalizeEachWord      :  require('./modules/__capitalizeEachWord'),
 };
+
+
